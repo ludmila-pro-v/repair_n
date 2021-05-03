f3ffe913e5843a7b5826cefea0d7409eb1ea5e08
ludmila-pro <luma02@mail.ru>
2021-05-03 18:35:25 +0500

    feat: add slider to  project section

--- js/main.js
+++ js/main.js
@@ -78,4 +78,23 @@ $(document).ready(function () {
             $('body,html').animate({scrollTop: 0}, 400);    
         });
     });
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+          },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+    });
+
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+
+    next.css('left', prev.width() + 10 + bullets.width() + 10)
+    bullets.css('left', prev.width() + 10)
 });
